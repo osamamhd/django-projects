d9d6a269c3ca3590ad9426aaad48eae909e8b656
Osama Mhd <osamamhd82@gmail.com>
2021-01-16 17:29:54 +0200

    home page url path.

--- instaclone/config/urls.py
+++ instaclone/config/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings # new
+from django.urls import path, include # new
+from django.conf.urls.static import static # new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('posts.urls')),
 ]
+
+if settings.DEBUG: # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
